b66eeaaf695ae430e62d06823af30ac8e774adcc
kippum03 <kippum03@naver.com>
2025-07-28 16:41:37 -0700

    Set up multiplayer spawning and local-only player movement with camera

---
+++ Assets/_Project/Scripts/Networking/NetworkManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public string gameVersion = "1.0";
+
+    void Start()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Photon Master Server");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("No room found, creating one...");
+        PhotonNetwork.CreateRoom(null, new RoomOptions());
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined room, spawning player...");
+
+        Vector3 spawnPosition = new Vector3(Random.Range(-5f, 5f), 1f, Random.Range(-5f, 5f));
+        PhotonNetwork.Instantiate("PlayerPrefab", spawnPosition, Quaternion.identity);
+    }
+}
---
+++ Assets/_Project/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviourPun
+{
+    public float moveSpeed = 5f;
+    public float turnSpeed = 180f;
+
+    private CharacterController controller;
+    private Camera playerCamera;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        if (photonView.IsMine)
+        {
+            // Attach the main camera to this player
+            playerCamera = Camera.main;
+            playerCamera.transform.SetParent(transform);
+            playerCamera.transform.localPosition = new Vector3(0, 2, -5);
+            playerCamera.transform.localRotation = Quaternion.Euler(10, 0, 0);
+        }
+    }
+
+    void Update()
+    {
+        if (!photonView.IsMine) return;
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.forward * v * moveSpeed;
+        controller.Move(move * Time.deltaTime);
+
+        transform.Rotate(0, h * turnSpeed * Time.deltaTime, 0);
+    }
+}
