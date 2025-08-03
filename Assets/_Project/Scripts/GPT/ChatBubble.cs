8b4abb276baf11f0ac7867fc29bf8aa4ab8315d0
kippum03 <kippum03@naver.com>
2025-08-02 19:27:42 -0700

    Connect ChatBubble.cs to prefab

---
+++ Assets/_Project/Scripts/GPT/ChatBubble.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ChatBubble : MonoBehaviour
+{
+    public TextMeshProUGUI chatText;
+
+    public void ShowMessage(string message, float duration = 5f)
+    {
+        chatText.text = message;
+        gameObject.SetActive(true);
+        StopAllCoroutines(); // Cancel any existing hide timers
+        StartCoroutine(HideAfterSeconds(duration));
+    }
+
+    private IEnumerator HideAfterSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        chatText.text = "";
+        gameObject.SetActive(false);
+    }
+}
