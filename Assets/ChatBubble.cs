d5ce7bbdd39814f9877a8068b06c1be315b4e5fa
kippum03 <kippum03@naver.com>
2025-08-02 19:08:38 -0700

    Add ChatBubble prefab with floating text UI

---
+++ Assets/ChatBubble.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ChatBubble : MonoBehaviour
+{
+    public TextMeshProUGUI chatText;
+
+    public void SetText(string message)
+    {
+        chatText.text = message;
+    }
+}
