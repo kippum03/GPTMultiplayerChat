b7e68c8888a9025ccb4549902fc227f8b3379cb3
kippum03 <kippum03@naver.com>
2025-08-02 20:35:29 -0700

    Create Chat UI Panel

---
+++ Assets/_Project/Scripts/Networking/ChatManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class ChatManager : MonoBehaviourPun
+{
+    public TMP_InputField chatInput;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) && !string.IsNullOrWhiteSpace(chatInput.text))
+        {
+            string message = chatInput.text;
+            chatInput.text = "";
+            photonView.RPC("BroadcastMessage", RpcTarget.All, PhotonNetwork.NickName, message);
+        }
+    }
+
+    [PunRPC]
+    void BroadcastMessage(string sender, string message)
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject player in players)
+        {
+            if (player.TryGetComponent(out ChatBubble bubble))
+            {
+                bubble.ShowMessage($"{sender}: {message}");
+            }
+        }
+    }
+}
+
